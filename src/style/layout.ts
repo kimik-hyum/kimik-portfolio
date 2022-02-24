dd3693017693e82d7203c70f3a538fd850d8a4af
kimik-hyum <familykim01@naver.com>
2022-02-24 21:58:56 +0900

    page animation

--- src/style/layout.ts
+++ src/style/layout.ts
@@ -27,22 +27,42 @@ const nextStyle = css`
   right: 0;
   top: calc(100% - 300px);
   height: 300px;
-  width: 500px;
-  transform-origin: 100% 0%;
-  transform: rotate(-50deg) translate(40px, 50px);
+  width: 300px;
   background: #000;
-
+  transform: rotate(45deg) translate(225px, 0px);
+  &:after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    transform: rotate(135deg) translate(55px, 55px);
+    border: 1px solid #ddd;
+    background: linear-gradient(
+      0deg,
+      rgba(255, 255, 255, 1) 0%,
+      rgba(212, 212, 212, 1) 100%
+    );
+  }
+  &:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 10;
+    background: #000;
+    width: 100%;
+    height: 100%;
+  }
   a {
     color: #fff;
   }
 `;
 
 const onRotateStyle = css`
-  width: 200vw;
-  height: 100vh;
-  max-width: inherit;
-  transform: rotate(0deg) translate(100vw, calc(-100vh + 300px));
-  transition: 0.6s transform;
+  transform: rotate(45deg) translate(225px, 0px) scale(15);
+  transition: 1.5s transform;
 
   a {
     right: calc(100vw + 250px);
